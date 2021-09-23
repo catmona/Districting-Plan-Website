f522e5db7589332ea84da0914e6f3bf9fb686a2c
Albin Robert <arobert@cs.stonybrook.edu>
2021-09-22 21:16:10 -0400

    initial spring commit

---
+++ server/src/main/java/com/mavericks/server/ServerApplication.java
@@ -0,0 +1,16 @@
+package com.mavericks.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+
+
+	}
+
+}
+
