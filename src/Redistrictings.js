bec7094e51d96de7c33d0c1ac9d5ead84b8b493c
schook <schook@pocopoints.com>
2021-09-26 02:39:43 -0400

    add scrollable redistricting container

--- src/Redistrictings.js
+++ src/Redistrictings.js
@@ -4,8 +4,32 @@ import { Container } from 'react-bootstrap';
 function Redistrictings(props) {
     
     return(
-        <Container>
+        <Container id="redistrictings" className="scrollbar scrollbar-primary">
+            <p>Teststart</p>
             <p>Test</p>
+            <p>Test</p>
+            <p>Test</p>
+            <p>Test</p>
+            <p>Test</p>
+            <p>Test</p>
+            <p>Test</p>
+            <p>Test</p>
+            <p>Test</p>
+            <p>Test</p>
+            <p>Test</p>
+            <p>Test</p>
+            <p>Test</p>
+            <p>Test</p>
+            <p>Test</p>
+            <p>Test</p>
+            <p>Test</p>
+            <p>Test</p>
+            <p>Test</p>
+            <p>Test</p>
+            <p>Test</p>
+            <p>Test</p>
+            <p>Test</p>
+            <p>Testend</p>
         </Container>
     )
 }
