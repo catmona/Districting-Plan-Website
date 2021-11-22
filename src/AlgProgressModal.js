58ec638eaa5647974725b666a35525edaa4f4973
Catherine <goldyflakes@gmail.com>
2021-11-21 22:03:21 -0500

    fleshed out progress modal

--- src/AlgProgressModal.js
+++ src/AlgProgressModal.js
@@ -1,27 +1,119 @@
+import { CanvasJSChart } from 'canvasjs-react-charts';
 import React, { useRef, useEffect, useState } from 'react';
-import { Modal, Button, Form, ModalBody, ModalFooter} from 'react-bootstrap';
-import ModalHeader from 'react-bootstrap/esm/ModalHeader';
+import { Modal, Button, Row, Col, ProgressBar} from 'react-bootstrap';
 
 function AlgProgressModal(props) {
-   
+    const [numIterations, setNumIterations] = useState(0);
+    const [popEquality, setPopEquality] = useState(0); //TODO should grab this from stats
+    const [compactness, setCompactness] = useState(0); //this too
+    const [timeRunning, setTimeRunning] = useState(0);
+    const [estTimeRemaining, setEstTimeRemaining] = useState(0);
+    const [progressPercent, setProgressPrecent] = useState(0);
+    const chart = useRef(null);
+
+    var dataLength = 20;
+    var popEqualityDps = [];
+    var compactnessDps = [];
+
+    const options = {
+        theme: "dark1",
+        animationEnabled: true,
+        axisX: { title: "Iteration" },
+        dataPointWidth: 20,
+        data: [{
+            type: "line",
+            color: "#ffc107",
+            showInLegend: true,
+            name: "Population Equality Score",
+            dataPoints: popEqualityDps
+        },
+        {
+            type: "line",
+            color: "#17a2b8",
+            showInLegend: true,
+            name: "Compactness Score",
+            dataPoints: compactnessDps
+        }]
+    }
+
+    useEffect(() => {
+        //do other stuff
+
+
+        //update chart
+        if(!chart.current) return;
+
+        popEqualityDps.push({
+            x: numIterations,
+            y: popEquality
+        });
+        compactnessDps.push({
+            x: numIterations,
+            y: compactness
+        });
+
+        if(popEqualityDps.length > dataLength) {
+            popEqualityDps.shift();
+        }
+        if(compactnessDps.length > dataLength) {
+            compactnessDps.shift();
+        }
+
+        chart.current.chart.render()
+    });
+
     //TODO Change in measures
     //TODO num iterations
     //TODO time has been running
     //TODO est. time complete
     //TODO can the user pause? finish early? how do we resume?
-    
+
     return(
         <Modal {...props} size="lg" centered className="dark-modal">
-            <ModalHeader>
-                Algorithm progress
-            </ModalHeader>
-            <ModalBody>
-                
-            </ModalBody>
-            <ModalFooter>
-               
+            <Modal.Header>
+                <Modal.Title>
+                    Algorithm progress
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Row>
+                    <div className="progress-block">
+                        <h4 className="progress-label">Num Iterations: </h4>
+                        <h4 id="progress-iterations" className="progress-value">{numIterations}</h4>
+                    </div>
+                    <Col className="progress-block">
+                        <h4 className="progress-label">Current Population Equality: </h4>
+                        <h4 id="progress-equality" className="progress-value">{popEquality}</h4>
+                    </Col>
+                    <Col className="progress-block">
+                        <h4 className="progress-label">Current Compactness: </h4>
+                        <h4 id="progress-compactness" className="progress-value">{compactness}</h4>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="progress-graph"> {/* Pop Equality Graph */}
+                    <CanvasJSChart options = {options} id="progress-chart" ref={chart} height="900px" />   
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="progress-block">
+                        <h4 className="progress-label">Time Spent Equalizing: </h4>
+                        <h4 id="progress-time" className="progress-value">{timeRunning}</h4>
+                    </Col>
+                    <Col className="progress-block">
+                        <h4 className="progress-label">Est. Time Remaining: </h4>
+                        <h4 id="progress-time-remaining" className="progress-value">{estTimeRemaining}</h4>
+                    </Col>
+                </Row>
+                <Row>
+                    <div className="progress-bar">
+                        <ProgressBar animated now={progressPercent} label={`${progressPercent}%`} variant="success" />
+                    </div>
+                </Row>
+            </Modal.Body>
+            <Modal.Footer>    
                 <Button variant="danger" onClick = {props.onHide}>Cancel</Button>
-            </ModalFooter>
+            </Modal.Footer>
         </Modal>
     );
 }
