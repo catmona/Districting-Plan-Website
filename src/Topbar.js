3a5a024f0f142abde806fb4998ada5cfd705419c
Catherine <goldyflakes@gmail.com>
2021-11-22 17:37:50 -0500

    cleaned up topbar

--- src/Topbar.js
+++ src/Topbar.js
@@ -15,22 +15,18 @@ function Topbar(props) {
     });
 
     return (
-        <Container fluid id="topbar">
-            <Row>
-                <Col md={6}>
-                    <h2 className="topbar-text">I want to </h2> <h1 className="topbar-text"><em className="rainbow-text">redistrict</em></h1> <h2 className="topbar-text">...</h2>
-                </Col>
-
-                <Col md={5} lg={3}>
-                    <DropdownButton menuVariant="dark" title={stateName} id="state-dropdown">
-                        <Dropdown.Item onClick={() => {setState("WA");}} className='state-dropdown-option'>Washington</Dropdown.Item>
-                        <Dropdown.Item onClick={() => {setState("NV");}} className='state-dropdown-option'>Nevada</Dropdown.Item>
-                        <Dropdown.Item onClick={() => {setState("AR");}} className='state-dropdown-option'>Arkansas</Dropdown.Item>
-                    </DropdownButton>
-                </Col>
-            </Row>
-          
-        </Container>
+        <Row id="topbar">
+            <Col className="topbar-item">
+                <h2 className="topbar-text">I want to </h2> <h1 className="topbar-text"><em className="rainbow-text">redistrict</em></h1> <h2 className="topbar-text">...</h2>
+            </Col>
+            <Col className="topbar-item" xs={"auto"}>
+                <DropdownButton id="state-dropdown" menuVariant="dark" title={stateName}>
+                    <Dropdown.Item onClick={() => {setState("WA");}} className='state-dropdown-option'>Washington</Dropdown.Item>
+                    <Dropdown.Item onClick={() => {setState("NV");}} className='state-dropdown-option'>Nevada</Dropdown.Item>
+                    <Dropdown.Item onClick={() => {setState("AR");}} className='state-dropdown-option'>Arkansas</Dropdown.Item>
+                </DropdownButton>
+            </Col>
+        </Row>
     )
 }
 
