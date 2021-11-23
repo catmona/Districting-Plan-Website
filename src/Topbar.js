ba81f299bfe0d289f49e78d145f0912810518c4f
Catherine <goldyflakes@gmail.com>
2021-11-22 20:58:21 -0500

    set up topbar thing for showing what plan selected

--- src/Topbar.js
+++ src/Topbar.js
@@ -3,21 +3,27 @@ import { DropdownButton } from 'react-bootstrap';
 import { Dropdown } from 'react-bootstrap';
 import { Col } from 'react-bootstrap';
 import { Row } from 'react-bootstrap';
-import { Container } from 'react-bootstrap';
 
 function Topbar(props) {
-    //const [stateName, setStateName] = useState("");
     let stateName = props.stateName!==''?props.stateName:'Select State';
     let setState = props.setState;
 
-    useEffect(() =>{
-        //document.getElementById("state-name").textContent = stateName;
-    });
+    useEffect(() => {
+        if(props.planType == "")
+            document.getElementById("topbar-status").firstChild.style.display="none"
+        else
+        document.getElementById("topbar-status").firstChild.style.display="inline-block"
+    }, [props.planType])
 
     return (
         <Row id="topbar">
             <Col className="topbar-item">
                 <h2 className="topbar-text">I want to </h2> <h1 className="topbar-text"><em className="rainbow-text">redistrict</em></h1> <h2 className="topbar-text">...</h2>
+            </Col>
+            <Col className="topbar-item" id="topbar-status" xd={"auto"}>
+                <div>
+                    <h3 className="topbar-text">{props.planType}</h3>
+                </div>
             </Col>
             <Col className="topbar-item" xs={"auto"}>
                 <DropdownButton id="state-dropdown" menuVariant="dark" title={stateName}>
