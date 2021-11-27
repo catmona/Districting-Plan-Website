24213cea8934834b2d344ff904ff131b672720a3
Catherine <goldyflakes@gmail.com>
2021-11-27 01:53:34 -0500

    got districting modal client-server working!

--- src/DistrictingModal.js
+++ src/DistrictingModal.js
@@ -9,7 +9,7 @@ function DistrictingModal(props) {
         popWhite: 50, election: 2014, demPercent: 60, repPercent: 40
     }]);
 
-    let dropdownTitle = selectedDistrict != -1 ? "District " + selectedDistrict : 'Select District ';
+    let dropdownTitle = selectedDistrict != -1 ? "District " + (selectedDistrict+1) : 'Select District ';
 
     useEffect(() => {
         if(!props.data) return;
@@ -29,8 +29,12 @@ function DistrictingModal(props) {
             demPercent = parseFloat(demPercent.toFixed(2));
 
             let data = {
-                popAll: pop[i][3], popAfricanAmerican: pop[i][1], popAsian: pop[i][0], 
-                popWhite: pop[i][2], election: 2014, demPercent: demPercent, repPercent: repPercent 
+                popAll: Number(pop[i][3]).toLocaleString(), 
+                popAfricanAmerican: Number(pop[i][1]).toLocaleString(), 
+                popAsian: Number(pop[i][0]).toLocaleString(), 
+                popWhite: Number(pop[i][2]).toLocaleString(), 
+                election: elections[i].info.year,
+                demPercent: demPercent, repPercent: repPercent 
             }
 
             list.push(data);
@@ -62,7 +66,7 @@ function DistrictingModal(props) {
                 <Row>
                     <DropdownButton id="districting-modal-dropdown" menuVariant="dark" title={dropdownTitle}>
                         {districtData.map((item, i) => {
-                            return <Dropdown.Item onClick={() => {setSelectedDistrict(i+1)}}>{"District " + (i+1)}</Dropdown.Item>
+                            return <Dropdown.Item onClick={() => {setSelectedDistrict(i)}}>{"District " + (i+1)}</Dropdown.Item>
                         })}
                     </DropdownButton>
                 </Row>
@@ -71,48 +75,48 @@ function DistrictingModal(props) {
                         <Row>
                             <p>Population: </p>
                             <div className="districting-modal-value">
-                                <p>8600</p>
+                                <p>{districtData[selectedDistrict] ? districtData[selectedDistrict].popAll : ""}</p>
                             </div>
                         </Row>
                         <Row>
                             <p>African American: </p>
                             <div className="districting-modal-value">
-                                <p>8600</p>
+                                <p>{districtData[selectedDistrict] ? districtData[selectedDistrict].popAfricanAmerican : ""}</p>
                             </div>
                         </Row>
                         <Row>
                             <p>Asian: </p>
                             <div className="districting-modal-value">
-                                <p>8600</p>
+                                <p>{districtData[selectedDistrict] ? districtData[selectedDistrict].popAsian : ""}</p>
                             </div>
                         </Row>
                         <Row>
                             <p>White: </p>
                             <div className="districting-modal-value">
-                                <p>8600</p>
+                                <p>{districtData[selectedDistrict] ? districtData[selectedDistrict].popWhite : ""}</p>
                             </div>
                         </Row>
                     </Col>
                     <Col id="districting-modal-congressional">
                         <Row className="districting-modal-congressional-block" id="districting-modal-election">
                             <h4>Election</h4>
-                            <p>2014</p>
+                            <p>{districtData[selectedDistrict] ? districtData[selectedDistrict].election : ""}</p>
                         </Row>
                         <Row className="districting-modal-congressional-block districting-modal-table">
                             <Row>
                                 <p>Democrat: </p>
                                 <div className="districting-modal-value">
-                                    <p id="districting-modal-dem">50%</p>
+                                    <p id="districting-modal-dem">{districtData[selectedDistrict] ? districtData[selectedDistrict].demPercent : ""}</p>
                                 </div>
                             </Row>
                             <Row>
                                 <p>Republican: </p>
                                 <div className="districting-modal-value">
-                                    <p id="districting-modal-rep">50%</p>
+                                    <p id="districting-modal-rep">{districtData[selectedDistrict] ? districtData[selectedDistrict].repPercent : ""}</p>
                                 </div>
                             </Row>
                             <div>
-                                <ProgressBar id="districting-modal-progress" now={50} variant="primary"/>
+                                <ProgressBar id="districting-modal-progress" now={districtData[selectedDistrict] ? districtData[selectedDistrict].demPercent : ""} variant="primary"/>
                             </div>
                         </Row>
                     </Col>
@@ -128,7 +132,7 @@ function DistrictingModal(props) {
                 >
                     Select
                 </Button>
-                <Button variant="danger" onClick = {props.onHide}>Close</Button>
+                <Button variant="danger" onClick={props.onHide}>Close</Button>
             </Modal.Footer>
 
         </Modal>
